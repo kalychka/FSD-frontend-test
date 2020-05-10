2fd606c3d858358bac4d9968a622154e0ffa153d
kolychka <randytaylor.fckoff@gmail.com>
2020-05-11 00:30:13 +0500

    Added jquery and jquery-ui, create dropdown

---
+++ components/dropdown/dropdown.js
@@ -0,0 +1,3 @@
+$( function() {
+    $("#dropdown").selectmenu().selectmenu("menuWidget");
+})
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('../node_modules/jquery-ui-dist/jquery-ui.js')
+require('../node_modules/jquery-ui-dist/jquery-ui.css')
 import './index.pug'
 import './styles/style.scss'
 import './components/text_field/text_field.scss'
+import './components/dropdown/dropdown.js'
+import './components/dropdown/dropdown.scss'
