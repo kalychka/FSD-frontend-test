5d6bd96f1015329759c05727ecf0a2ec44b9f2a9
kolychka <randytaylor.fckoff@gmail.com>
2020-05-13 18:23:08 +0500

    Added masked text field

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require('../node_modules/jquery-ui-dist/jquery-ui.js')
 require('../node_modules/jquery-ui-dist/jquery-ui.css')
+require('./plugins/jquery.maskedinput.min.js')
+
 import './index.pug'
 import './styles/style.scss'
+
 import './components/text_field/text_field.scss'
+
 import './components/dropdown/dropdown.js'
 import './components/dropdown/dropdown.scss'
+
+import './components/masked text field/masked_text.js'
+import './components/masked text field/masked_text.scss'
