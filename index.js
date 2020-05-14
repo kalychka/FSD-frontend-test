c35b2cc318ee8fd2667f25f5d56acbe3ae28a3a9
kolychka <randytaylor.fckoff@gmail.com>
2020-05-14 23:16:09 +0500

    init filter-dropdown

--- index.js
+++ index.js
@@ -18,5 +18,8 @@ import expand_more from './assets/expand_more.png'
 import './components/date dropdown/date_dropdown.js'
 import './components/date dropdown/date_dropdown_default.js'
 
+import './components/filter-date/filter-dropdown.scss'
+import './components/filter-date/filter-dropdown.js'
+
 
 
