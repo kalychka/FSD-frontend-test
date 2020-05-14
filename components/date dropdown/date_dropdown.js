5476fbd49a30608b55742c4b866fbe3f0d655cc4
kolychka <randytaylor.fckoff@gmail.com>
2020-05-14 22:58:25 +0500

    Localization datepicker

--- components/date dropdown/date_dropdown.js
+++ components/date dropdown/date_dropdown.js
@@ -6,6 +6,7 @@ $( function() {
         firstDay: 1,
         dateFormat: "dd.mm.yy",
         minDate: 0,
+        monthNames : ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
         dayNamesMin: [ "ВС", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ" ],
         duration: "fast",
         gotoCurrent: true,
