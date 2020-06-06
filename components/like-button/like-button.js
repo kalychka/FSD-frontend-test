8201f37ffb272637d90d7aa8e078e031c8d64f0f
kolychka <randytaylor.fckoff@gmail.com>
2020-06-06 15:09:24 +0500

    Complete like-buttons

--- components/like-button/like-button.js
+++ components/like-button/like-button.js
@@ -1,7 +1,17 @@
 $(function() {
     var like = 0;
-    $(".like-button__label").click(function() {
+    $("#like-one").click(function() {
         like += 1;
-        $(".btn-counter").text(like);
+        $('#like-one__counter').text(like);
+    })
+})
+
+$(function() {
+    var like = 0;
+    $("#like-two").click(function() {
+        like += 1;
+        $('#like-two__counter').text(like);
+        $("#like-two span").text("favorite");
+        $("#like-two").addClass("like-button__label_active")
     })
 })
