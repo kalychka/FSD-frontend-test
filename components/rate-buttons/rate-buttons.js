642ff4ed0bd2a84b652dea0addf943fa0b89fcc1
kolychka <randytaylor.fckoff@gmail.com>
2020-06-09 21:36:38 +0500

    Fix rate-buttons

--- components/rate-buttons/rate-buttons.js
+++ components/rate-buttons/rate-buttons.js
@@ -1,14 +1,15 @@
-$(function() {
-    $(".rate-buttons-list__item").click(function(){
-        var select = $('.rate-buttons-list__item').index(this);  
-        for (i = 0; i <= select; i++) {
-            $('.rate-buttons-list__item span').eq(i).text('star');
-        };
-    })
-    $(".rate-buttons-list__item").click(function(){
-        var select = $(".rate-buttons-list__item").index(this);  
-        for (i = 0; i <= select; i++) {
-            $('.rate-buttons-list__item span').eq(i).text('star');
-        };
-    })
+$("#rate-one li").click(function(){  
+    var select = $('#rate-one li').index(this);  
+    console.log(select);
+    for (i = 0; i <= select; i++) {
+        $('#rate-one li span').eq(i).text('star');
+    };
+})
+
+$("#rate-two li").click(function(){  
+    var select = $('#rate-two li').index(this);  
+    console.log(select);
+    for (i = 0; i <= select; i++) {
+        $('#rate-two li span').eq(i).text('star');
+    };
 })
