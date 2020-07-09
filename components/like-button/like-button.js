2226fc1c9074e9333a0584de1b8b3c8af6e61668
kolychka <randytaylor.fckoff@gmail.com>
2020-07-09 22:59:33 +0500

    Rewrite like-button script

--- components/like-button/like-button.js
+++ components/like-button/like-button.js
@@ -1,17 +1,28 @@
 $(function() {
+    // объявление переменной лайков
     var like = 0;
+    // клик по label 
     $("#like").click(function() {
+        // увеличение числа лайков на один
         like += 1;
-        $('#like__counter').text(like);
+        // запись числа в ссылку
+        $(this).find('#like__counter').text(like);
     })
 })
 
+
 $(function() {
+    // объявление переменной лайков
     var like = 0;
+    // клик по label 
     $("#like-active").click(function() {
+        // увеличение числа лайков на один
         like += 1;
-        $('#like-active__counter').text(like);
-        $("#like-active span").text("favorite");
-        $("#like-active").addClass("like-button__label_active")
+        // запись числа в ссылку
+        $(this).find('#like-active__counter').text(like);
+        // изменение спрайта
+        $(this).find("span").text("favorite");
+        // добавление модификатора
+        $(this).addClass("like-button__label_active")
     })
 })
