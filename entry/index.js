5e25ea988c7307134633a021ffe6ff983eb6737c
kolychka <randytaylor.fckoff@gmail.com>
2020-07-16 16:48:18 +0500

    Rewrite structure

---
+++ entry/index.js
@@ -0,0 +1,6 @@
+require('@/plugins/jquery-ui-dist/jquery-ui.js')
+require('@/plugins/jquery-ui-dist/jquery-ui.css')
+require('@/plugins/jquery.maskedinput.min.js')
+require('@/plugins/jquery.datepicker.extension.range.min.js')
+
+import '@/styles/style.scss'
---
+++ entry/ui-kit.js
@@ -0,0 +1,52 @@
+require('./index.js')
+
+import '@/pages/ui-kit.pug'
+import '@/styles/ui-kit.scss'
+
+import '@components/text_field/text_field.scss'
+
+import '@components/dropdown/dropdown.js'
+import '@components/dropdown/dropdown.scss'
+
+
+import '@components/masked text field/masked_text.js'
+import '@components/masked text field/masked_text.scss'
+
+import '@components/date dropdown/date_dropdown.scss'
+import '@components/date dropdown/date_dropdown.js'
+
+import '@components/filter-date/filter-dropdown.scss'
+import '@components/filter-date/filter-dropdown.js'
+
+import '@components/subscriptions-text-field/subscriptions-text-field.scss'
+
+import '@components/checkbox-buttons/checkbox-buttons.scss'
+
+import '@components/radio-buttons/radio-buttons.scss'
+
+import '@components/toggle/toggle.scss'
+
+import '@components/like-button/like-button.js'
+import '@components/like-button/like-button.scss'
+
+import '@components/rate-buttons/rate-buttons.scss'
+import '@components/rate-buttons/rate-buttons.js'
+
+import '@components/slider-range/slider-range.scss'
+import '@components/slider-range/slider-range.js'
+
+import '@components/buttons/button.scss'
+
+
+import '@components/pagination/pagination.scss'
+
+import '@components/expandable-checkbox-list/expandable-checkbox-list.scss'
+import '@components/expandable-checkbox-list/expandable-checkbox-list.js'
+
+import '@components/rich-checkbox/rich-checkbox.scss'
+
+import '@components/bullet-list/bullet-list.scss'
+
+import '@components/icon-text-list/icon-text-list.scss'
+
+import '@components/comment/comment.scss'
