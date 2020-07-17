902b6e420cf422ee76ad97d24348dd3941b08019
kolychka <randytaylor.fckoff@gmail.com>
2020-07-17 11:35:50 +0500

    Added search room form

---
+++ entry/ui-kit2.js
@@ -0,0 +1,15 @@
+require('./index.js')
+
+import '@/pages/ui-kit2.pug'
+import '@/styles/ui-kit2.scss'
+
+import '@components/search-room/search-room.scss'
+
+import '@components/date dropdown/date_dropdown.js'
+import '@components/date dropdown/date_dropdown.scss'
+
+import '@components/dropdown/dropdown.js'
+import '@components/dropdown/dropdown.scss'
+
+import '@components/buttons/button.scss'
+
