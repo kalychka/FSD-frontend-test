f78881db1e0019f3394f87224dd84ee34b6c1727
kolychka <randytaylor.fckoff@gmail.com>
2020-07-19 14:27:01 +0500

    Refactoring buttons title and dropdown margins

--- entry/index.js
+++ entry/index.js
@@ -2,5 +2,7 @@ require('@/plugins/jquery-ui-dist/jquery-ui.js')
 require('@/plugins/jquery-ui-dist/jquery-ui.css')
 require('@/plugins/jquery.maskedinput.min.js')
 require('@/plugins/jquery.datepicker.extension.range.min.js')
+require('@/plugins/pickmeup.min.js')
+require('@/plugins/pickmeup.css')
 
 import '@/styles/style.scss'
