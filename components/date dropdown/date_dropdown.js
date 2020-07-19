c447c51505f0b434a7269b6c264e15fb08dde4c8
kolychka <randytaylor.fckoff@gmail.com>
2020-07-19 23:25:05 +0500

    Update date-dropdown

--- components/date dropdown/date_dropdown.js
+++ components/date dropdown/date_dropdown.js
@@ -1,20 +1,37 @@
 $(function() {
-    $('#datepicker__start').datepicker({
+    $('.datepicker-from').datepicker({
         range: true,
         multipleDatesSeparator: '-',
-        classes: 'date_dropdown',
+        classes: 'date-dropdown',
         clearButton: true,
-        
         prevHtml: 'arrow_back',
         nextHtml: 'arrow_forward',
         todayButton: true,
         navTitles: {
             days: '<h2>MM yyyy</h2>'
         },
+        language: {
+            today: 'Применить'
+        },
+        onRenderCell: function(date, cellType) {
+            if (cellType == 'day') {
+                return {
+                    html: `${date.getDate()}<div class="bg-selected"><span></span></div>`
+                }
+            }
+        },
         onSelect: function (fd, d, picker) { 
-            $("#datepicker__start").val(fd.split("-")[0]);
-            $("#datepicker__end").val(fd.split("-")[1]);
-          }
+            $(".datepicker-from").val(fd.split("-")[0]);
+            $(".datepicker-to").val(fd.split("-")[1]);  
+        }
+    })
+    $(".datepicker").find(".datepicker--button").click(function() {
+        if (this.dataset.action === 'today') {
+            $(this).parents('.date-dropdown').removeClass('active');
+        }
+    })
+    $('.datepicker-to').click(function() {
+        $(this).parents('.date-dropdown__container').find('.datepicker-from').data('datepicker').show();
     })
 })
 
