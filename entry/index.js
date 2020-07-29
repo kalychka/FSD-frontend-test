be86a03d363add6510209a86c52411709cbc7d93
kolychka <randytaylor.fckoff@gmail.com>
2020-07-29 23:10:22 +0500

    Remove old plugins

--- entry/index.js
+++ entry/index.js
@@ -1,7 +1,6 @@
-require('@/plugins/jquery-ui-dist/jquery-ui.js')
-require('@/plugins/jquery-ui-dist/jquery-ui.css')
+require('@/plugins/jquery.min.js')
+
 require('@/plugins/jquery.maskedinput.min.js')
-require('@/plugins/jquery.datepicker.extension.range.min.js')
 
 require('@/plugins/air-datepicker/dist/css/datepicker.min.css')
 require('@/plugins/air-datepicker/dist/js/datepicker.min.js')
