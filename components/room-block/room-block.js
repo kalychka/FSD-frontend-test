fe3bcabe028da76f77eb653d070b617cc85d172b
kolychka <randytaylor.fckoff@gmail.com>
2020-07-29 20:07:02 +0500

    Refactoring room block

--- components/room-block/room-block.js
+++ components/room-block/room-block.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-    $('.room-block__carousel').slick({
-        autoplay: true,
-        appendArrows: $(this).find('.room-block__carousel'),
-        prevArrow: '<div class="room-block__button prevArrow">expand_more</div>',
-        nextArrow: '<div class="room-block__button nextArrow">expand_more</div>',
-        dots: true,
-        dotsClass: 'room-dots',
-    });
+    $('.room-block').each(function() {
+        $(this).find('.room-block__carousel').slick({
+            autoplay: true,
+            appendArrows: $(this).find('.room-block__carousel'),
+            prevArrow: '<div class="room-block__button prevArrow">expand_more</div>',
+            nextArrow: '<div class="room-block__button nextArrow">expand_more</div>',
+            dots: true,
+            dotsClass: 'room-dots',
+        });
+    })
   });
+
