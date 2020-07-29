120f38f3910455b7c74dbf86db4a1f535b2ef7e3
kolychka <randytaylor.fckoff@gmail.com>
2020-07-29 22:55:31 +0500

    Refactoring rate-buttons and small fix

--- components/rate-buttons/rate-buttons.js
+++ components/rate-buttons/rate-buttons.js
@@ -1,10 +1,12 @@
-$("#rate-buttons .rate-buttons-list__item").click(function(){  
-
-    var select = $(this).index(); 
+$('.rate-buttons-list').each(function() {
+    var select = 
     console.log(select)
     for (i = 0; i <= select; i++) {
         $(this).parent().find('.rate-buttons-list__item span').eq(i).text('star');
     };
+}) 
 
-})
+
+
+
 
