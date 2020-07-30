34f6937c868ae6a9b9a0fb92f65d5dc03c97a1d3
kolychka <randytaylor.fckoff@gmail.com>
2020-07-30 22:45:29 +0500

    Added landing page

---
+++ entry/landing.js
@@ -0,0 +1,17 @@
+require('@/entry/index.js')
+
+import '@/pages/landing-page.pug'
+import '@/styles/landing-page.scss'
+
+import '@components/header/header.scss'
+import '@components/buttons/button.scss'
+
+import '@components/search-room/search-room.scss'
+
+import '@components/dropdown/dropdown.scss'
+import '@components/dropdown/dropdown.js'
+
+import '@components/date dropdown/date_dropdown.scss'
+import '@components/date dropdown/date_dropdown.js'
+import '@components/footer/footer.scss'
+import '@components/subscriptions-text-field/subscriptions-text-field.scss'
